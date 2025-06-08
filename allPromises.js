c9a8b92d682d57a11f2223f93a4c12f362ab2aef
kamufozi <kamufozi679@gmail.com>
2025-06-08 16:43:43 +0200

    Updated the allpromise tasks

--- allPromises.js
+++ allPromises.js
@@ -1,29 +1,29 @@
-// const promise1=new Promise((_,resolve)=>{
-//     // console.log('hey')
-//     resolve('done1');
-// });
-// const promise2=new Promise((resolve)=>{
-//     // console.log('hey')    
-//     resolve('done2')
-//     });
-// const promise3=new Promise((reject)=>{
-//     // console.log('hey3')
-//     reject('done3')
-// });
-// const promiseOuch= Promise.all([promise1,promise2,promise3]);
-// // console.log(promiseOuch);
-// promiseOuch.then(results=>
-//     console.log(results)
-// ).catch(err=>
-//     console.error('wa mbwa we',err)
-// )
-// const dodumba= Promise.allSettled([promise1,promise2,promise3])
-// // console.log(dodumba)
-// dodumba.then(resul=>console.log(resul)).catch(failor=>console.log(failor))
+const promise1=new Promise((_,resolve)=>{
+    // console.log('hey')
+    resolve('done1');
+});
+const promise2=new Promise((resolve)=>{
+    // console.log('hey')    
+    resolve('done2')
+    });
+const promise3=new Promise((reject)=>{
+    // console.log('hey3')
+    reject('done3')
+});
+const promiseOuch= Promise.all([promise1,promise2,promise3]);
+// console.log(promiseOuch);
+promiseOuch.then(results=>
+    console.log(results)
+).catch(err=>
+    console.error('wa mbwa we',err)
+)
+const dodumba= Promise.allSettled([promise1,promise2,promise3])
+// console.log(dodumba)
+dodumba.then(resul=>console.log(resul)).catch(failor=>console.log(failor))
 
-// const fastasfuckboi= Promise.race([promise1,promise2,promise3]);
-// console.log(fastasfuckboi);
-// fastasfuckboi.then(resul=>console.log(resul)).catch(erroro=>console.error(`Je fail j ai l admit parce que:${erroro}`))
+const fastasfuckboi= Promise.race([promise1,promise2,promise3]);
+console.log(fastasfuckboi);
+fastasfuckboi.then(resul=>console.log(resul)).catch(erroro=>console.error(`Je fail j ai l admit parce que:${erroro}`))
 
 Promise.resolve()
     .then(() => console.log(1));
